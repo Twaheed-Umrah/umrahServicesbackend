4b76c7a99fc7d1bdeea2c9c33037e39fe14aa568
Shuru63 <shubhamkumargarg63@gmail.com>
2025-07-08 23:41:00 +0530

    update in /hajjumarhlead/serializers.py         modified:   config/settings.py

--- apps/hajjumarhlead/serializers.py
+++ apps/hajjumarhlead/serializers.py
@@ -21,7 +21,7 @@ class HajjUmrahBookingServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = HajjUmrahBookingService
         fields = [
-            'id', 'selected_date', 'selected_time', 'first_name', 'last_name',
+            'id', 'first_name', 'last_name',
             'email', 'phone', 'business_plan', 'created_at', 'updated_at'
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
